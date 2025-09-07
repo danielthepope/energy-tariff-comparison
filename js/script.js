0fccb4898cc387a4e3f11c281b6f30abfd2057a6
Dan Pope <danielthepope@users.noreply.github.com>
2025-09-07 19:48:04 +0100

    Add your daily usage

--- js/script.js
+++ js/script.js
@@ -11,6 +11,7 @@ function processForm(_) {
     const currentStandingCharge = parseFloat(document.getElementById('current-standing-charge').value);
     const alternativeUnitRate = parseFloat(document.getElementById('alternative-unit-rate').value);
     const alternativeStandingCharge = parseFloat(document.getElementById('alternative-standing-charge').value);
+    const averageUsage = parseFloat(document.getElementById('daily-usage').value);
 
     if (isNaN(currentUnitRate) || isNaN(currentStandingCharge) || isNaN(alternativeUnitRate) || isNaN(alternativeStandingCharge)) {
         // Let's bail
@@ -19,7 +20,15 @@ function processForm(_) {
     }
 
     const breakEven = (currentStandingCharge - alternativeStandingCharge) / (alternativeUnitRate - currentUnitRate);
-    document.getElementById('result-text').innerHTML = `The break even point is <strong>${breakEven.toFixed(2)}</strong> kWh`;
+    document.getElementById('result-text').innerHTML = `The break even point is <strong>${breakEven.toFixed(2)}</strong> kWh.`;
+
+    if (!isNaN(averageUsage)) {
+        if (breakEven < averageUsage) {
+            document.getElementById('result-text').innerHTML += ' Since you use more energy than the break-even point, the new tariff works out better for you.';
+        } else {
+            document.getElementById('result-text').innerHTML += ' Since you use less energy than the break-even point, your current tariff works out better for you.';
+        }
+    }
 }
 
 document.querySelectorAll('input[inputmode=numeric]').forEach(e => e.addEventListener('change', numericValidate));
