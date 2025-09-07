5e6e720a85dfb5283936120ed4a074f59c1c0d9d
Dan Pope <danielthepope@users.noreply.github.com>
2025-09-07 21:20:58 +0100

    Shoehorn chart.js in there

--- js/script.js
+++ js/script.js
@@ -29,8 +29,124 @@ function processForm(_) {
             document.getElementById('result-text').innerHTML += ' Since you use less energy than the break-even point, your current tariff works out better for you.';
         }
     }
+
+    updateChart(currentStandingCharge, breakEven, currentUnitRate, averageUsage, alternativeStandingCharge, alternativeUnitRate);
 }
 
-document.querySelectorAll('input[inputmode=numeric]').forEach(e => e.addEventListener('change', numericValidate));
-document.querySelectorAll('input[inputmode=numeric]').forEach(e => e.addEventListener('change', processForm));
+function updateChart(currentStandingCharge, breakEven, currentUnitRate, averageUsage, alternativeStandingCharge, alternativeUnitRate) {
+    if (isNaN(averageUsage)) {
+        averageUsage = 0;
+    }
+    const currentPoints = [{
+        x: 0,
+        y: currentStandingCharge
+    }, {
+        x: breakEven,
+        y: currentStandingCharge + (breakEven * currentUnitRate)
+    }, {
+        x: averageUsage,
+        y: currentStandingCharge + (averageUsage * currentUnitRate)
+    }, {
+        x: (averageUsage + breakEven) * 3,
+        y: currentStandingCharge + ((averageUsage + breakEven) * 3 * currentUnitRate)
+    }];
+    currentPoints.sort(p => p.x);
+    const alternativePoints = [{
+        x: 0,
+        y: alternativeStandingCharge
+    }, {
+        x: breakEven,
+        y: alternativeStandingCharge + (breakEven * alternativeUnitRate)
+    }, {
+        x: averageUsage,
+        y: alternativeStandingCharge + (averageUsage * alternativeUnitRate)
+    }, {
+        x: (averageUsage + breakEven) * 3,
+        y: alternativeStandingCharge + ((averageUsage + breakEven) * 3 * alternativeUnitRate)
+    }];
+    alternativePoints.sort(p => p.x);
+    chart.data.datasets[0].data = currentPoints;
+    chart.data.datasets[1].data = alternativePoints;
+    chart.options.scales.x.max = Math.max(breakEven * 2, averageUsage * 1.5);
+    chart.options.scales.y.max = Math.max(
+        (alternativeStandingCharge + (breakEven * alternativeUnitRate)) * 2,
+        (currentStandingCharge + (breakEven * currentUnitRate)) * 2,
+        (alternativeStandingCharge + (averageUsage * alternativeUnitRate)) * 1.5,
+        (currentStandingCharge + (averageUsage * currentUnitRate)) * 1.5
+    );
+    chart.data.datasets[2].data = [
+        {
+            x: breakEven,
+            y: -10
+        },
+        {
+            x: breakEven,
+            y: Math.max(...currentPoints.map(p => p.y), ...alternativePoints.map(p => p.y)) * 2
+        }
+    ];
+    chart.data.datasets[3].data = [
+        {
+            x: averageUsage,
+            y: -10
+        },
+        {
+            x: averageUsage,
+            y: Math.max(...currentPoints.map(p => p.y), ...alternativePoints.map(p => p.y)) * 2
+        }
+    ];
+    chart.update();
+}
+
+const ctx = document.getElementById('myChart');
+const chart = new Chart(ctx, {
+    type: 'scatter',
+    data: {
+        datasets: [{
+            label: 'Current',
+            data: [],
+            borderWidth: 1,
+            showLine: true
+        }, {
+            label: 'New',
+            data: [],
+            borderWidth: 1,
+            showLine: true
+        }, {
+            label: 'Break even',
+            data: [],
+            borderWidth: 1,
+            showLine: true
+        }, {
+            label: 'Daily usage',
+            data: [],
+            borderWidth: 1,
+            showLine: true
+        }]
+    },
+    options: {
+        scales: {
+            x: {
+                beginAtZero: true,
+                min: 0,
+                max: 10,
+                title: {
+                    display: true,
+                    text: 'Daily usage in kWh'
+                }
+            },
+            y: {
+                beginAtZero: true,
+                min: 0,
+                max: 10,
+                title: {
+                    display: true,
+                    text: 'Price in pence'
+                }
+            }
+        }
+    }
+});
+
+document.querySelectorAll('input[inputmode=numeric]').forEach(e => e.addEventListener('keyup', numericValidate));
+document.querySelectorAll('input[inputmode=numeric]').forEach(e => e.addEventListener('keyup', processForm));
 processForm();
